de5c9139156c6d8c0dcd405cc0da72992ac43d94
George Matthew L <matthew.limongan@gmail.com>
2020-12-22 14:37:15 +0700

    Fix timer issue

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -4,7 +4,7 @@ import questionAPI from "../Question";
 import QuestionBox from "../Question/QuestionBox";
 import Result from "../Question/ResultBox";
 
-export class Quiz extends React.Component  {
+export class Quiz extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -14,12 +14,11 @@ export class Quiz extends React.Component  {
       correctlyAnsweredQuestionId: [],
       finalScore: 0,
       minute: 0,
-      second: 2,
+      second: 10,
     };
 
     this.handleSubmit.bind(this);
   }
-
 
   getQuestions = () => {
     questionAPI().then((question) => {
@@ -34,9 +33,10 @@ export class Quiz extends React.Component  {
       correctQuestionAnswered: 0,
       correctlyAnsweredQuestionId: [],
       finalScore: 0,
-      minute:0,
-      second:2,
+      minute: 0,
+      second: 10,
     });
+    this.timer();
   };
 
   computeAnswer = (answer, correctAnswer, questionId) => {
@@ -63,40 +63,37 @@ export class Quiz extends React.Component  {
 
   timer = () => {
     this.myInterval = setInterval(() => {
-      const { second, minute } = this.state
+      const { second, minute } = this.state;
 
       if (second > 0) {
         this.setState(({ second }) => ({
-          second: second - 1
-        }))
+          second: second - 1,
+        }));
       }
       if (second === 0) {
         if (minute === 0) {
-          clearInterval(this.myInterval)
+          clearInterval(this.myInterval);
+          this.handleSubmit();
         } else {
           this.setState(({ minute }) => ({
             minute: minute - 1,
-            second: 59
-          }))
+            second: 59,
+          }));
         }
       }
-    }, 1000)
-  }
+    }, 1000);
+  };
 
   componentDidMount() {
     this.getQuestions();
     this.timer();
-
   }
 
   componentWillUnmount() {
-    clearInterval(this.myInterval)
+    clearInterval(this.myInterval);
   }
 
   handleSubmit() {
-    console.log("test 1")
-    this.componentWillUnmount();
-    console.log("test 2")
     const {
       correctQuestionAnswered,
       questionBank: { length },
@@ -126,52 +123,54 @@ export class Quiz extends React.Component  {
     });
 
     this.setState({ submitted: true, finalScore });
-    this.forceUpdate();
   }
 
   render() {
     const { questionBank, submitted, finalScore, minute, second } = this.state;
 
-
     return (
       <Box>
         <h2>Pop Quiz!</h2>
-        <h3>Time remaining: {minute}:{second< 10 ? `0${ second }` : second}</h3>
-        { minute === 0 && second === 0
-            ?
-            <div>{this.handleSubmit()}
-            {submitted && <Result score={finalScore} playAgain={this.playAgain} />}</div>
-            : <div>{!submitted &&
-          questionBank.length > 0 &&
-          questionBank.map(
-            ({ question, answers, correct, questionId }, index) => (
-              <QuestionBox
-                key={questionId}
-                index={index + 1}
-                question={question}
-                options={answers}
-                selected={(answer) =>
-                  this.computeAnswer(answer, correct, questionId)
-                }
+        {!submitted && (
+          <h3>
+            Time remaining: {minute}:{second < 10 ? `0${second}` : second}
+          </h3>
+        )}
+
+        <div>
+          {!submitted &&
+            questionBank.length > 0 &&
+            questionBank.map(
+              ({ question, answers, correct, questionId }, index) => (
+                <QuestionBox
+                  key={questionId}
+                  index={index + 1}
+                  question={question}
+                  options={answers}
+                  selected={(answer) =>
+                    this.computeAnswer(answer, correct, questionId)
+                  }
+                />
+              )
+            )}
+
+          {!submitted && (
+            <Box align="end">
+              <Button
+                primary
+                label="Submit"
+                onClick={() => this.handleSubmit()}
               />
-            )
+            </Box>
           )}
-              {!submitted && (
-                  <Box align="end">
-                    <Button
-                        primary
-                        label="Submit"
-                        onClick={() => this.handleSubmit()}
-                    />
-                  </Box>
-              )}
 
-              {submitted && <Result score={finalScore} playAgain={this.playAgain} />}
-
+          {submitted && (
+            <Result score={finalScore} playAgain={this.playAgain} />
+          )}
         </div>
-        }
       </Box>
     );
   }
 }
+
 export default Quiz;
